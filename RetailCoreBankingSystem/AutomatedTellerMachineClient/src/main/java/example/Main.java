47802c8426a1cd49bbf90261fcb66101c584263d
Huiren Woo <giantcrabby@gmail.com>
2020-10-23 01:09:15 +0800

    Add ATM client

---
+++ RetailCoreBankingSystem/AutomatedTellerMachineClient/src/main/java/example/Main.java
@@ -0,0 +1,9 @@
+package example;
+
+import javax.naming.InitialContext;
+
+public class Main {
+    public static void main(String a[]) throws Exception {
+        InitialContext ic = new InitialContext();
+    }
+}
